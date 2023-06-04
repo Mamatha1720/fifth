00028428a93072faf02ad0204bb080e4accf476f
Mamatha1720 <mamathat1621@gmail.com>
2023-06-04 21:07:00 +0530

    new one

---
+++ script.js
@@ -0,0 +1,9 @@
+localStorage.setItem('name','Bob')
+localStorage.removeItem('name')
+
+sessionStorage.setItem('name','John')
+sessionStorage.setItem('name','Bob'))
+
+document.cookie='name=kyle; expires=' + new Date(9999, 0, 1).toUTCString()
+
+document.cookie='last=kyle; expires=' + new Date(9999, 0, 1).toUTCString()
